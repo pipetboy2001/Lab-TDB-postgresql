12e1e09240869475cb1dbdb3af045d0bfad3e04f
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-09-08 04:19:54 -0300

    Bootstrap in Read instituciones
    a la vista de leer se le añade bootstrap

--- interfaz-app-grupo4/src/App.vue
+++ interfaz-app-grupo4/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <table>
-      <tbody>
-        <tr v-for="todos in todos" :key="todos.id">
-          <td>{{ todos.id }}</td>
-          <td>{{ todos.nombre }}</td>
-          <td>{{ todos.descrip }}</td>
+    <h1>Instituciones CRUD</h1>
+    <button type="button" class="btn btn-primary">Añadir</button>
+
+    <!--Bootstrp table-->
+    <table class="table table-striped table-dark table-bordered table-secondary">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">NOMBRE</th>
+          <th scope="col">DESCRIPCION</th>
+          <th scope="col">ACCIONES</th>
         </tr>
+      </thead>
+      <tbody>
+          <tr scope="row" v-for="todos in todos" :key="todos.id">
+            <td>{{ todos.id }}</td>
+            <td>{{ todos.nombre }}</td>
+            <td>{{ todos.descrip }}</td>
+            <td> 
+              <!--Botones-->
+              <button type="button" class="btn btn-warning">Editar</button>
+              <button type="button" class="btn btn-danger">Borrar</button> </td>
+          </tr>
       </tbody>
     </table>
+
+
+
   </div>
 </template>
 
