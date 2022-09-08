790f8cacee7cccec2c970a4982b53fe887ac79e5
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-09-08 03:59:32 -0300

    Interfaz app grupo4 READ
    se crea un listado de lo que se encuentra

---
+++ interfaz-app-grupo4/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="todos in todos" :key="todos.id">
+          <td>{{ todos.id }}</td>
+          <td>{{ todos.nombre }}</td>
+          <td>{{ todos.descrip }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+  data(){
+    return {
+      todos:null
+    }
+  },
+  mounted() {
+    console.log('hola xd mounted')
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      console.log('aca va el codgio de gets')
+      axios
+        .get('http://localhost:3000/instituciones')
+        .then(response => {
+          console.log(response)
+          this.todos = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      }
+    }
+}
+</script>
+
+<style>
+</style>
