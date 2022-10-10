775a578f167f23b9eb92aefceecd36f262169758
Alen Galindo <alen.galindo@usach.cl>
2022-10-09 23:13:34 -0300

    Agregados scripts de creacion y carga de datos

--- api-rest-app-grupo4/src/main/java/com/tingesogrupo4/apirestappgrupo4/services/RankingService.java
+++ api-rest-app-grupo4/src/main/java/com/tingesogrupo4/apirestappgrupo4/services/RankingService.java
@@ -1,2 +1,43 @@
-package com.tingesogrupo4.apirestappgrupo4.services;public class RankingService {
+package com.tingesogrupo4.apirestappgrupo4.services;
+
+import com.tingesogrupo4.apirestappgrupo4.models.Emergencia;
+import com.tingesogrupo4.apirestappgrupo4.models.Ranking;
+import com.tingesogrupo4.apirestappgrupo4.repositories.RankingRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class RankingService {
+
+    RankingRepository rankingRepository;
+    @PostMapping("/ranking/create")
+    public Ranking createRanking(@RequestBody Ranking ranking){
+        System.out.println("Intento posting ranking...");
+        return rankingRepository.createRanking(ranking);
+    }
+    @GetMapping("/ranking/{id}")
+    public Ranking getRankingById(@PathVariable Integer id){
+        return rankingRepository.getRankingById(id);
+    }
+
+    @GetMapping("/ranking")
+    public List<Ranking> getAllRanking(){
+        return rankingRepository.getAllRankings();
+    }
+
+    @PutMapping("/ranking/{id}")
+    public Ranking updateRanking(@RequestBody Ranking ranking){
+        return rankingRepository.updateRanking(ranking);
+    }
+
+    @DeleteMapping("/ranking/{id}")
+    public void deleteRankingById(@PathVariable Integer id){
+        rankingRepository.deleteRankingById(id);
+    }
+
+    @DeleteMapping("/ranking")
+    public void deleteRanking(){
+        rankingRepository.deleteRanking();
+    }
+
 }
